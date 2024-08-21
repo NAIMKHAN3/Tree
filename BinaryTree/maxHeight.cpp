a4e5ab77e3115dd1510d265a177046064be10da0
Naim Khan <naimkhan01856@gmail.com>
2024-08-21 11:14:03 +0600

    max height done

---
+++ BinaryTree/maxHeight.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int v)
+    {
+        val = v;
+        left = right = NULL;
+    }
+};
+
+int maxHeight(Node *root)
+{
+    if (root == NULL)
+        return 0;
+    int l = maxHeight(root->left);
+    int r = maxHeight(root->right);
+    return max(l,r) + 1;
+}
+
+int main()
+{
+    Node *root = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    Node *e = new Node(60);
+    Node *f = new Node(70);
+    Node *g = new Node(80);
+    Node *h = new Node(90);
+    Node *i = new Node(100);
+    root->right = a;
+    root->left = b;
+    b->right = d;
+    b->left = e;
+    d->right = i;
+    d->left = h;
+    e->left = g;
+    a->left = c;
+    c->right = f;
+   int v = maxHeight(root);
+   cout << v << endl;
+    return 0;
+}
