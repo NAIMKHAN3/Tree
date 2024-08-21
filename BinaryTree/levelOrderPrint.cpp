5792eb2edc09e09fabd3aac75a2a77c2c9033602
Naim Khan <naimkhan01856@gmail.com>
2024-08-21 10:03:38 +0600

    level order print done

---
+++ BinaryTree/levelOrderPrint.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int val;
+    Node* left;
+    Node* right;
+    Node(int v){
+        val = v;
+        left = right = NULL;
+    }
+};
+
+void levelOrderPrint (Node* root){
+    if(root == NULL) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        cout << temp->val << " ";
+        if(temp->left) q.push(temp->left);
+        if(temp->right) q.push(temp->right);
+    }
+}
+
+int main()
+{
+    Node * root = new Node(10);
+    Node * a = new Node(20);
+    Node * b = new Node(30);
+    Node * c = new Node(40);
+    Node * d = new Node(50);
+    Node * e = new Node(60);
+    Node * f = new Node(70);
+    Node * g = new Node(80);
+    Node * h = new Node(90);
+    Node * i = new Node(100);
+    root->right = a;
+    root->left = b;
+    b->right= d;
+    b->left = e;
+    d->right = i;
+    d->left = h;
+    e->left = g;
+    a->left= c;
+    c->right= f;
+    levelOrderPrint(root);
+    return 0;
+}
